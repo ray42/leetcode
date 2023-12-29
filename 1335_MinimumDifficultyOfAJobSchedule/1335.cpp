2b8e26a754791156954bd488183d510a30dae91d
ray <raymonwhite42@gmail.com>
2023-12-29 18:22:23 +0000

    Added comment

---
+++ 1335_MinimumDifficultyOfAJobSchedule/1335.cpp
@@ -0,0 +1,2 @@
+Use this solution: https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/solutions/4472328/beats-1st-90-02-2nd-99-04-users-dp-c-java-python-javascript-with-explanation
+The first one exceeds time limit for C++
