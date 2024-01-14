7c958b0cf92444ebaf541235af0a572c632344eb
ray <raymonwhite42@gmail.com>
2024-01-14 18:33:45 +0000

    Removed the questions since I think they are copywrited

--- 0271_EncodeAndDecodeStrings/0271.cpp
+++ 0271_EncodeAndDecodeStrings/0271.cpp
@@ -1,27 +1,6 @@
 /*
 Medium Premium: https://www.lintcode.com/problem/659/
 
-Description
-
-Design an algorithm to encode a list of strings to a string. 
-The encoded string is then sent over the network and is decoded back to the original list of strings.
-
-Please implement encode and decode.
-
-Example1
-
-Input: ["lint","code","love","you"]
-Output: ["lint","code","love","you"]
-Explanation:
-One possible encode method is: "lint:;code:;love:;you"
-
-Example 2
-
-Input: ["we", "say", ":", "yes"]
-Output: ["we", "say", ":", "yes"]
-Explanation:
-One possible encode method is: "we:;say:;:::;yes"
-
 RAYNOTES
 We COULD use a delimiter, but what if the delimiter is a part of the input?
 E.g. if we choose # as our delimeter, and if the input is:
--- 1897_RedistributeCharactersToMakeAllStringsEqual/1897.cpp
+++ 1897_RedistributeCharactersToMakeAllStringsEqual/1897.cpp
@@ -1,30 +1,6 @@
 /*
 EASY https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/description/
 
-You are given an array of strings words (0-indexed).
-In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
-Return true if you can make every string in words equal using any number of operations, and false otherwise.
-
-Example 1:
-
-Input: words = ["abc","aabc","bc"]
-Output: true
-Explanation: Move the first 'a' in words[1] to the front of words[2],
-to make words[1] = "abc" and words[2] = "abc".
-All the strings are now equal to "abc", so return true.
-
-Example 2:
-
-Input: words = ["ab","a"]
-Output: false
-Explanation: It is impossible to make all the strings equal using the operation.
-
-Constraints:
-    1 <= words.length <= 100
-    1 <= words[i].length <= 100
-    words[i] consists of lowercase English letters.
-
-
 RayNotes
   OKAY, so the 1 operation is to basically move 1 character from any position in string i to any position in string j
   Given such a free and unrestricted operation, if it is possible to make all strings equal, then we CAN make all strings equal.
