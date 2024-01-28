61e5b64b1415ccff8fc83cd65655338c6d5880b9
ray <raymonwhite42@gmail.com>
2024-01-28 13:38:12 +0000

    Putting all neetcode in sheets

--- setup.py
+++ setup.py
@@ -1,22 +1,38 @@
 import sys
 import os
 
-# This program prints Hello, world!
-dirname = sys.argv[1]
 
-if os.path.exists(dirname):
-    print(dirname, ' exists')
-else:
-    os.mkdir(dirname)
-    print(dirname, ' created')
 
-os.chdir(dirname);
 
-filename = dirname[0:4] + '.cpp'
-if not os.path.exists(filename):
-    open(filename, 'a').close()
-    print(filename, ' created')
-else:
-    print(filename, ' exists')
+def setupLeetCode(dirname):
+    if os.path.exists(dirname):
+        print(dirname, ' exists')
+    else:
+        os.mkdir(dirname)
+
+    os.chdir(dirname);
+
+    filename = dirname[0:4] + '.cpp'
+    if not os.path.exists(filename):
+        open(filename, 'a').close()
+    else:
+        print(filename, ' exists')
+
+def setupMultipleLeetCode(listFileName):
+    with open(listFileName) as file:
+        while line := file.readline():
+            setupLeetCode(line.strip())
+
+def main():
+    name = sys.argv[1]
+    if(name == "list.txt"):
+        setupMultipleLeetCode(name)
+    else:
+        setupLeetCode(name)
+        
+if __name__ == "__main__":
+    main()
+
+
 
 
