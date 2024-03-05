02d432f013f40bcea896ea8ed4056aebd126facb
ray <raymonwhite42@gmail.com>
2024-03-05 22:08:36 +0000

    added daily

---
+++ 1750_MinimumLengthOfStringAfterDeletingSimilarEnds/1750.cpp
@@ -0,0 +1,31 @@
+#include <string>
+
+class Solution 
+{
+public:
+    auto minimumLength(const std::string& s) -> int
+    {
+        // Return the minimum length after removing similar ends
+        // 1 <= s.length <= 105
+        // s only consists of characters 'a', 'b', and 'c'.
+
+        // Using two pointe, we 
+        auto left = 0;
+        auto right = s.size() - 1;
+
+        //while(left < right)
+        //{
+        //    if
+        //}
+
+        return right - left;
+    }
+};
+
+
+auto main(int argc, char* argv[]) -> int
+{
+    
+    return 0;
+}
+
