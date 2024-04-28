bf819b7138418849a036233af0b301d83789fa89
ray <raymonwhite42@gmail.com>
2024-04-28 23:53:57 +0100

    Keep safe

--- 0217_ContainsDuplicate/0217.cpp
+++ 0217_ContainsDuplicate/0217.cpp
@@ -5,10 +5,12 @@ Easy https://leetcode.com/problems/contains-duplicate/
 
 #include <vector>
 #include <unordered_set>
+#include <type_traits>
+
 bool containsDuplicate(std::vector<int>& nums) 
 {
     // So if there is a duplicate in nums, return true. Otherwise return false.
-    auto umap = std::unordered_set<int>{};
+    auto umap = std::unordered_set<std::remove_cvref_t<decltype(nums)>::value_type>{};
     for(const auto& i : nums)
     {
         if(!umap.insert(i).second)
