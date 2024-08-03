55f669d3490d9c03c77386693a5b1f70d534b4c0
ray <raymonwhite42@gmail.com>
2024-08-03 12:10:17 +0100

    Done 0019_RemoveNthNodeFromEndOfList 0143_ReorderList

--- 0019_RemoveNthNodeFromEndOfList/0019.cpp
+++ 0019_RemoveNthNodeFromEndOfList/0019.cpp
@@ -17,92 +17,102 @@ struct ListNode
 // head =  [1,2,3,4,5], n = 2
 // Output: [1,2,3,  5]
 //
-// We go to the end to see how many nodes there are.
-// Let sz be the number of nodes. Then we can do sz - n to get the prev node of the one we want to remove.
-// E.g. 5-2 = 3, so we go to the third node, and set it's next pointer to point to the next next pointer.
-// Edge cases: What happens at n=5 (remove the head) and n=1 (remove the last node)?
-// if n = 5, then we have 5-5 = 0, in this case, we need to set the head to the head's next node.
-// if n = 1, then we have 5-1 = 4, then we set the 4th node's next pointer to the next next pointer (which is null) - this is no different than the normal case.s
-class Solution1 {
+// 1) With the help of a dummyHead, count the number of nodes:
+// dummyHead->1->2->3->4->5->null
+//  Staring at dummyHead, we can go until current->next is null, incrementing a counter.
+//
+// 2) The node previous to the node we want to remove will be "nnode - n", in the example: 5 - 2 = 3,
+// dummyHead->1->2->3->4->5->null
+//                
+// Staring at dummyHead, we step nnode-n = 3 times.
+//
+// 3) Perform the removal:
+//    current->next = current->next->next
+//
+// Edge cases:
+// The above approach works for removing the first and last node (corresponding to n = 5 and n = 1 in the above example)
+class SolutionTwoPass {
 public:
+    using PtrListNode = ListNode*;
     auto removeNthFromEnd(ListNode* head, int n) -> ListNode*
     {
-        // Just in case we have a null head.
-        if(!head) return head;
-
-        // Count the number of nodes
-        auto sz = 0;
-        auto temp = head;
-        do
+        // With the help of a dummyHead, count the number of nodes
+        auto dummyHead = ListNode{0,head};
+        auto current = &dummyHead;
+        auto nnodes = int{0};
+        while(current->next)
         {
-            ++sz;
-            temp = temp->next;
-        }
-        while(temp);
-
-        auto prevNodeIndex = sz - n;
-
-        // This works for head = [1], n = 1, we return null in this case.
-        if(prevNodeIndex == 0) return head->next;
-
-        auto dummyNode = ListNode{0, head};
-        auto ptrDummyNode = &dummyNode;
-
-        // Now go to the previous node (to the one we want to remove) and remove it.
-        while(prevNodeIndex > 0)
-        {
-            ptrDummyNode = ptrDummyNode->next;
-            --prevNodeIndex;
+            ++nnodes;
+            current = current->next;
         }
 
-        ptrDummyNode->next = ptrDummyNode->next->next;
-        
-        return head;
+        // Move to the node previous to the one we want to remove.
+        auto prevNodeNum = nnodes - n;
+        current = &dummyHead;
+        while(prevNodeNum > 0)
+        {
+            current = current->next;
+            --prevNodeNum;
+        }
+
+        // Do the deletion
+        current->next = current->next->next;
+
+        return dummyHead.next;
     }
 };
 
 // One pass, two pointer solution.
-// head =  [1,2,3,4,5]
-// if n = 0, then prev = 5
-// if n = 1, then prev = 4
-// if n = 4, then prev = 1
-// if n = 5, then prev = the node before 1 (this is a special case).
-// Note that prev is n position away from the last node. So what we can do is maintain two pointers, n nodes apart.
-// Then, once the fast pointer reaches the last node, we know that the slow pointer is at the correct prev position.
+// Input:  dH->1->2->3->4->5->null n = 2
+// Output: dH->1->2->3  -> 5->null
+//
+// 1)
+// Use two pointers, start at a dummyHead and increment the second pointer by n.
+// dH->1->2->3->4->5->null
+// ^      ^ (increment pointer by 2)
 // 
-// NOTE: We don't even need to do the special case if we return dummy.next, which is what I have done.
-// How do we handle the special case?
-// When placing the slow and fast pointers, we start a dummy node which points to the head.
-// If n = 5, then we move the fast pointer 5 times to point to 5, then we can check it's next pointer, if it's nullptr, then we have reach the special case.
-//     Then we simply return head->next
-class Solution2 {
+// 2)
+// Now we move them both until the incremented pointer's next is null. 
+// The first pointer will be at the previous node to the one which we want to remove:
+// 
+// dH->1->2->3->4->5->null
+//           ^     ^
+// 
+// 3) Now perform the removal for the pointer at 3.
+// current->next = current->next->next
+class SolutionTwoPointer {
 public:
+using PtrListNode = ListNode*;
     auto removeNthFromEnd(ListNode* head, int n) -> ListNode*
     {
-        if(!head) return head;
+        auto dummy = ListNode{0,head};
 
-        auto dummy = ListNode{0, head};
-        auto fast = &dummy;
-        auto slow = &dummy;
-        while(n > 0)
+        auto first = &dummy;
+        auto second = &dummy;
+        // 1) Move the second pointer by n
+        while(n>0)
         {
-            fast = fast->next;
+            second = second->next;
             --n;
         }
 
-        // Start shifting both pointers until fast->next is nullptr
-        while(fast->next)
+        // 2) Move both pointers until the second pointer reaches the end of the list
+        while(second->next)
         {
-            slow = slow->next;
-            fast = fast->next;
+            first = first->next;
+            second = second->next;
         }
 
-        // Perform the removal
-        slow->next = slow->next->next;
+        // 3) Remove the node after first
+        first->next = first->next->next;
 
+        // Return the head. Note that the original head could have been removed, so we need to return
+        // dummy.next
         return dummy.next;
     }
 };
+
+
 
 
 
@@ -114,7 +124,7 @@ auto main(int argc, char* argv[]) -> int
     auto n2 = std::make_shared<ListNode>(2, n3.get());
     auto n1 = std::make_shared<ListNode>(1, n2.get());
 
-    auto ii = Solution2{}.removeNthFromEnd(n1.get(),2);
+    auto ii = SolutionTwoPointer{}.removeNthFromEnd(n1.get(),2);
 
     return 0;
 }
--- 0143_ReorderList/0143.cpp
+++ 0143_ReorderList/0143.cpp
@@ -134,6 +134,100 @@ using PtrListNode = ListNode*;
             return;
         }
 
+        // Need to do three things:
+        // 1) Split the linked list,
+        // 2) Reverse right half
+        // 3) Combine
+        
+
+
+        // 1) Split the linked list,
+        auto slow = head;
+        auto fast = head->next;
+
+        // How did we derive this condition? Let's check out where the fast pointer lands in both even and odd number cases:
+        // 1->2->3->4
+        //          ^ fast ends here
+        //    ^ slow ends here
+        // 1->2->3->4->5
+        //               ^ fast ends at a nullptr
+        //       ^slow ends here
+        // 
+        // So in both cases, we can terminate the while loop and guarantee that the slow pointer's next is the start of the 2nd half of the list
+        // if the condition is fast->next != nullptr (for even) and fast != nullptr (odd)
+        while(fast && fast->next)
+        {
+            // Advance slow by 1
+            slow = slow->next;
+
+            // Advance fast by two, this is guaranteed to work, try it with slow and fast pointer with a 
+            // odd and even numbered list. We will never go out of bounds.
+            // Think of it like this, if fast is currently on a not null node, or on null, then the loop condition triggers and we won't run anymore.
+            // this means, fast must be at the penultimate node or earlier. If it's on the penultimate, then
+            // fast->next is the last node and fast->next->next is nullptr, both are valid.
+            fast = fast->next->next;
+        }
+
+
+        // 2) Reverse right half
+
+        // Now we reverse the 2nd half of the list, which starts at slow->next
+        auto rhsCurrent = slow->next;
+
+        // Terminate the LHS
+        slow->next = nullptr;
+        
+        auto prev = PtrListNode{nullptr};
+        while(rhsCurrent)
+        {
+            auto next = rhsCurrent->next;
+            rhsCurrent->next = prev;
+            prev = rhsCurrent;
+            rhsCurrent = next;
+        }
+
+        // Now combine these two!
+        auto lhsHead = head;
+        auto rhsHead = prev;
+        
+        // This is pretty damn smart. I works if you try on an odd and then an even list:
+        // Even:
+        // 1->2->null
+        // 4->3
+        //   On the last iteration, point 2 to 3, then 3 to what 2 WAS pointing to (null)
+        //
+        // Odd:
+        // 1->2->3
+        // 4->5
+        //   On the last iteration, point 2 to 5, then 5 to what 2 WAS pointing to (3). 3 is already pointing to null, so we are done!
+        while(rhsHead)
+        {
+            auto next1 = lhsHead->next;
+            auto next2 = rhsHead->next;
+            
+            lhsHead->next = rhsHead;
+            rhsHead->next = next1;
+
+            lhsHead = next1;
+            rhsHead = next2;
+        }
+    }
+};
+
+// This is my first attempt at the solution. After watching NeetCode's video, I realised that my approach was
+// not element, so I am trying again, see the solution above.
+class SolutionOptimisedSpaceFirstAttempt {
+public:
+using PtrListNode = ListNode*;
+    auto reorderList(ListNode* head) -> void
+    {
+        // Edge cases for 
+        // [], [1], [1,2], only when we reach [1,2,3] is when re-ordering happens: [1,3,2]
+        if(head == nullptr || head->next == nullptr || head->next->next == nullptr)
+        {
+            return;
+        }
+
         // 1) Split the linked list,
         // 2) Reverse right half
         // 3) Combine
@@ -163,6 +257,9 @@ using PtrListNode = ListNode*;
 
         // Now we reverse the 2nd half of the list, which starts at slow->next
         auto rhsCurrent = slow->next;
+
+        // Terminate the RHS
+        slow->next = nullptr;
         
         auto prev = PtrListNode{nullptr};
         while(rhsCurrent)
@@ -177,21 +274,42 @@ using PtrListNode = ListNode*;
         auto rhsHead = prev;
         auto lhsHead = head;
         
-        //auto tmp = ListNode{};
-        //auto prev = &tmp;
-        //while(lhsHead || rhsHead)
-        //{
-        //    prev->next = lhsHead;
-        //    lhsHead->next = rhsHead;
-        //}
+        auto tmp = ListNode{};
+        prev = &tmp;
+        while(lhsHead)
+        {
+            auto next1 = lhsHead->next;
+            prev->next = lhsHead;
 
-        
+            if(rhsHead == nullptr)
+            {
+                prev = lhsHead;
+                break;
+            }
+            auto next2 = rhsHead->next; // this is where the nullptr access happened.
+            prev->next->next = rhsHead;
+
+            prev = rhsHead;
+            lhsHead = next1;
+            rhsHead = next2;
+        }
+
+        prev->next = nullptr;
     }
 };
 
 
 auto main(int argc, char* argv[]) -> int
 {
+    auto l5 = ListNode{5};
+    auto l4 = ListNode{4, &l5};
+    auto l3 = ListNode{3, &l4};
+    auto l2 = ListNode{2, &l3};
+    auto l1 = ListNode{1, &l2};
+    SolutionOptimisedSpace{}.reorderList(&l1);
+
+    auto ii = 42;
+
     return 0;
 }
 
